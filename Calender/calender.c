e0a86df0817b5ab09adfde9e6689f792e020ba14
Habibu Mamudu <habibumamudu@gmail.com>
2023-05-10 03:46:55 +0000

    calender

---
+++ Calender/calender.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("May 2023");
+
+    printf("\nSun\tMon\tTues\tWed\tThurs\tFri\tSat");
+    printf("\n");
+    printf("\n\t1\t2\t3\t4\t5\t6");
+    printf("\n");
+    printf("\n7\t8\t9\t10\t11\t12\t13");
+    printf("\n");
+    printf("\n14\t15\t16\t17\t18\t19\t20");
+    printf("\n");
+    printf("\n21\t22\t23\t24\t25\t26\t27");
+    printf("\n");
+    printf("\n28\t29\t30\t31");
+
+    return 0;
+}
