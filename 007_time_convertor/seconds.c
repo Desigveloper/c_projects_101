ae1b69cfcdaa82c9d93173225b9538302a7838b7
Habibu Mamudu <habibumamudu@gmail.com>
2023-05-24 11:42:07 +0000

    Guess  game

--- 007_time_convertor/seconds.c
+++ 007_time_convertor/seconds.c
@@ -9,8 +9,11 @@
 #include <stdio.h>
 
 #define SECONDS_PER_MIN 60
-#define SECONDS_PER_HOUR (SECONDS_PER_MIN * 60)
-#define SECONDS_PER_DAY (SECONDS_PER_HOUR * 24)
+#define MINS_PER_HOUR 60
+#define HOURS_PER_DAY 24
+#define SECONDS_PER_HOUR (SECONDS_PER_MIN * MINS_PER_HOUR)
+#define SECONDS_PER_DAY (SECONDS_PER_HOUR * HOURS_PER_DAY)
+
 
 typedef unsigned int integer;
 
@@ -19,7 +22,7 @@ void convert_time(integer);
 int main(void)
 {
     integer iSeconds = 0;
-    printf("Enter number of seconds ie. <1000000>: ");
+    printf("Enter number of seconds ie. <1 to 210, 000>: ");
     scanf("%d", &iSeconds);
     
     convert_time(iSeconds);
@@ -33,7 +36,7 @@ void convert_time(integer seconds) {
     iDays = seconds / SECONDS_PER_DAY;
     iHours = seconds / SECONDS_PER_HOUR;
     iMinutes = seconds / SECONDS_PER_MIN;
-    iHoursLeft = iDays % SECONDS_PER_HOUR;
+    iHoursLeft = (iHours % SECONDS_PER_MIN) - (iDays * HOURS_PER_DAY);
     iMinutesLeft = iMinutes % SECONDS_PER_MIN;
     iSecondsLeft = seconds % SECONDS_PER_MIN;
 
