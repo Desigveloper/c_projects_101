e7d247d9c1a568981014b40cb4239845ca7d0a25
Habibu Mamudu <habibumamudu@gmail.com>
2023-05-26 15:09:23 +0000

    update arith quiz app

--- 009_arithmetic_quiz/random_math.c
+++ 009_arithmetic_quiz/random_math.c
@@ -1,7 +1,9 @@
 /**
  * Program: random_math.c
- * Purpose: The program allows the user to select the type of arithmetic operation and the number
- * number of question then the program generates random question to be solved
+ * Purpose: The program allows the user to select the type of arithmetic
+ * operation and the number question then the program generates random
+ * questions based on the age of the user to be solved
+ * 
  * By: Habib(desigveloper)
  * Date: 25/05/2023
  */
@@ -10,18 +12,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define YES 1
-#define NO 0
-
+//Custom function Prototypes
 void printMenuList();
 int makeSelection(int iResponse);
-void generateAdditions();
-void generateSubtractions();
-void generateMultiplications();
-void generateDivisions();
-void generateModulo();
+void generateRandomNumbers();
+void generateQuestions(void);
+void generateAdditions(int iUserAge);
+void generateSubtractions(int iUserAge);
+void generateMultiplications(int iUserAge);
+void generateDivisions(int iUserAge);
+void generateModulo(int iUserAge);
 
-int iRandNum_1, iRandNum_2,iAnswer, iMenuOption, iNumberOfQuestions, iChoice;
+//Global variables
+int iRandNum_1, iRandNum_2,iAnswer, iMenuOption;
+int iNumberOfQuestions, iChoice, iUserAge;
 int iPoints = 0;
 
 int main(void)
@@ -30,44 +34,23 @@ int main(void)
     srand(time(NULL));
 
     printf("\t\tTHE RANDOM ARITHMETIC MATH QUIZ GENERATOR\n");
-    printf("\nEnter number of questions to ask: ");
-    scanf("%d", &iNumberOfQuestions);
-    
-
+    printf("\n\nARITHMETIC OPTIONS\n\n");
     printMenuList();
-    scanf("%d", &iMenuOption);
 
-    iChoice = makeSelection(iMenuOption);
+    printf("\n\nChoose and option 1 - 5\n");
 
-    switch (iChoice)
-    {
-        case 1:
-            generateAdditions();
-            break;
-        case 2:
-            generateSubtractions();
-            break;
-        case 3:
-            generateMultiplications();
-            break;
-        case 4:
-            generateDivisions();
-            break;
-        case 5:
-            generateModulo();
-            break;
-    }
+    generateQuestions();
 
     if (iPoints == iNumberOfQuestions)
-        printf("\nCongratulations! You scored %d/%d/n", iPoints, iNumberOfQuestions);
+        printf("\nCongratulations! You scored %d/%d\n", iPoints, iNumberOfQuestions);
     else
         printf("\nYou scored %d/%d\n", iPoints, iNumberOfQuestions);
     return 0;
 }
 
+//Prints the arithmetic operator list
 void printMenuList()
 {
-    printf("\t\tChoose and option 1 - 5\n");
     printf("1. Additions (+)\n");
     printf("2. Subtractions (-)\n");
     printf("3. Multiplications (x)\n");
@@ -102,12 +85,72 @@ int makeSelection(int iResponse)
     return iChoice;
 }
 
-void generateAdditions()
+//Generates highest random number based on users age
+void generateRandomNumbers(void)
+{
+
+    if (iUserAge < 12)
+    {
+        iRandNum_1 = rand() % 100;
+        iRandNum_2 = rand() % 100;
+    }
+    else if (iUserAge < 17)
+    {
+        iRandNum_1 = rand() % 500;
+        iRandNum_2 = rand() % 500;
+    }
+    else if (iUserAge < 24)
+    {
+        iRandNum_1 = rand() % 1000;
+        iRandNum_2 = rand() % 1000;
+    }
+    else
+    {
+        iRandNum_1 = rand() % 10000;
+        iRandNum_2 = rand() % 10000;
+    }
+}
+
+//Generate random arithmetic questions based on the operator 
+// the user selects from the list and the numbers of questions.
+void generateQuestions()
+{
+    scanf("%d", &iMenuOption);
+
+    iChoice = makeSelection(iMenuOption);
+
+    printf("\nEnter number of questions to ask: ");
+    scanf("%d", &iNumberOfQuestions);
+
+    //Get user age
+    printf("\nWhat's your age? ");
+    scanf("%d", &iUserAge);
+
+    switch (iChoice)
+    {
+        case 1:
+            generateAdditions(iUserAge);
+            break;
+        case 2:
+            generateSubtractions(iUserAge);
+            break;
+        case 3:
+            generateMultiplications(iUserAge);
+            break;
+        case 4:
+            generateDivisions(iUserAge);
+            break;
+        case 5:
+            generateModulo(iUserAge);
+            break;
+    }
+}
+
+void generateAdditions(int iUserAge)
 {
     for (int i = 0; i < iNumberOfQuestions; i++)
         {
-            iRandNum_1 = rand() % 10000;
-            iRandNum_2 = rand() % 10000;
+            generateRandomNumbers();
 
             printf("\nWhat is %d + %d: ", iRandNum_1, iRandNum_2);
             scanf("%d", &iAnswer);
@@ -125,12 +168,11 @@ void generateAdditions()
         }
 }
 
-void generateSubtractions()
+void generateSubtractions(int iUserAge)
 {
     for (int i = 0; i < iNumberOfQuestions; i++)
         {
-            iRandNum_1 = rand() % 10000;
-            iRandNum_2 = rand() % 10000;
+            generateRandomNumbers();
 
             printf("\nWhat is %d - %d: ", iRandNum_1, iRandNum_2);
             scanf("%d", &iAnswer);
@@ -148,12 +190,11 @@ void generateSubtractions()
         }
 }
 
-void generateMultiplications()
+void generateMultiplications(int iUserAge)
 {
     for (int i = 0; i < iNumberOfQuestions; i++)
         {
-            iRandNum_1 = rand() % 10000;
-            iRandNum_2 = rand() % 100;
+            generateRandomNumbers();
 
             printf("\nWhat is %d * %d: ", iRandNum_1, iRandNum_2);
             scanf("%d", &iAnswer);
@@ -171,12 +212,11 @@ void generateMultiplications()
         }
 }
 
-void generateDivisions()
+void generateDivisions(int iUserAge)
 {
     for (int i = 0; i < iNumberOfQuestions; i++)
         {
-            iRandNum_1 = rand() % 10000;
-            iRandNum_2 = rand() % 500;
+            generateRandomNumbers();
 
             printf("\nWhat is %d/%d: ", iRandNum_1, iRandNum_2);
             scanf("%d", &iAnswer);
@@ -194,12 +234,11 @@ void generateDivisions()
         }
 }
 
-void generateModulo()
+void generateModulo(int iUserAge)
 {
     for (int i = 0; i < iNumberOfQuestions; i++)
         {
-            iRandNum_1 = rand() % 1000;
-            iRandNum_2 = rand() % 500;
+            generateRandomNumbers();
 
             printf("\nWhat is %d modulo %d: ", iRandNum_1, iRandNum_2);
             scanf("%d", &iAnswer);
